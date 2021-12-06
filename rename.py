aa30fc32d9e9e927537d9ee1abd4d17f7b4df42f
huangming <huangming@bdstar.com>
2021-12-06 17:11:53 +0800

    optimize more condition

--- rename.py
+++ rename.py
@@ -6,10 +6,14 @@ import functools
 import re
 
 preName = "up_"
-path = r'D:\noboPic\运动模式序列置修改_水温表遮挡\运动模式表盘起来修改'
+newPreName = "loop_"
+path = r'D:\noboPic\赛道模式序列置修改_水温表遮挡\赛道模式表盘起来修改'
+newSubNumberCount = 3
 
 
 def getIndex(x: str):
+    if len(preName) == 0 :
+        return int(re.findall(r'(.+?).png', x)[0])
     return int(re.findall(r'%s(.+?).png' % preName, x)[0])
 
 
@@ -24,19 +28,19 @@ def cmp(x: str, y: str):
     return 0
 
 
-old_dir = os.listdir(path)  # 获取/home/linuxidc/linuxidc.com目录下的所有文件目录
+old_dir = os.listdir(path)
 print("原始目录为 %s" % old_dir)
 old_dir.sort(key=functools.cmp_to_key(cmp))
 # 重命名
-# for i in old_dir:
-#     if not i.endswith('.png'):
-#         continue
-#     new_name = '%s%s.png' % (preName, str(getIndex(i)).rjust(5, '0'))
-#     os.rename(path + "/" + i, path + "/" + new_name)
-# new_dir = os.listdir(path)
-# print("现在的目录为%s" % new_dir)
+for i in old_dir:
+    if not i.endswith('.png'):
+        continue
+    new_name = '%s%s.png' % (newPreName, str(getIndex(i)).rjust(newSubNumberCount, '0'))
+    os.rename(path + "/" + i, path + "/" + new_name)
+new_dir = os.listdir(path)
+print("现在的目录为%s" % new_dir)
 
 # 删除多余的
-for i in old_dir:
-    if getIndex(i) % 2 > 0:
-        os.remove(path + "/" + i)
+# for i in old_dir:
+#     if getIndex(i) % 2 > 0:
+#         os.remove(path + "/" + i)
