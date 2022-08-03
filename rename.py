944f2291893f2c911f42c34b6d5649f097bf2547
huangming <huangming@bdstar.com>
2022-08-03 14:26:45 +0800

    添加重新设置角标

--- rename.py
+++ rename.py
@@ -5,10 +5,11 @@ import os
 import functools
 import re
 
-preName = "up_"
-newPreName = "loop_"
-path = r'D:\noboPic\赛道模式序列置修改_水温表遮挡\赛道模式表盘起来修改'
+preName = "100-50_"
+newPreName = "end_"
+path = r'I:\svn\长安\75A121\Doc\09 UIUE\仪表\动效\S202MCA动效\赛道模式\赛道模式上下坡视频序列帧\赛道0-100过渡循环序列_220719\赛道0-100过渡循环序列_220719\4-1.100-50过渡序列'
 newSubNumberCount = 3
+isReIndex = 1
 
 
 def getIndex(x: str):
@@ -32,10 +33,15 @@ old_dir = os.listdir(path)
 print("原始目录为 %s" % old_dir)
 old_dir.sort(key=functools.cmp_to_key(cmp))
 # 重命名
+index = 0
 for i in old_dir:
     if not i.endswith('.png'):
         continue
-    new_name = '%s%s.png' % (newPreName, str(getIndex(i)).rjust(newSubNumberCount, '0'))
+    if isReIndex:
+        new_name = '%s%s.png' % (newPreName, str(index).rjust(newSubNumberCount, '0'))
+    else:
+        new_name = '%s%s.png' % (newPreName, str(getIndex(i)).rjust(newSubNumberCount, '0'))
+    index += 1
     os.rename(path + "/" + i, path + "/" + new_name)
 new_dir = os.listdir(path)
 print("现在的目录为%s" % new_dir)
